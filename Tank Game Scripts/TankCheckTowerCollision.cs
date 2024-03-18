d346d2216be88bfce4ad732b110810c8aff93afc
Shippinh <logoshit@gmail.com>
2024-03-18 18:34:02 +0200

    Fixed references to Destroyed gameObjects

--- Tank Game Scripts/TankCheckTowerCollision.cs
+++ Tank Game Scripts/TankCheckTowerCollision.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TankCheckTowerCollision : MonoBehaviour
 {
     void OnCollisionEnter(Collision col)
     {
-        //this still needs more work, all those nullReferences are not good
-        if(col.collider.tag == "Player Bullet" && transform.parent.GetComponent<TankEnemyBehaviour>().isDestroyed != true)
+        if(transform != null)
         {
-            TankEnemyBehaviour thisTowersTank = transform.parent.GetComponent<TankEnemyBehaviour>();
-            float destroyTime = thisTowersTank.destroyTime; //+1f = yield wait in destruction function of enemy tank
-            thisTowersTank.CollisionDetected(this);
-            Destroy(this.gameObject, destroyTime);
+            if(transform.parent != null)
+            {
+                if(transform.parent.GetComponent<TankEnemyBehaviour>() != null)
+                {
+                    if(col.collider.tag == "Player Bullet" && transform.parent.GetComponent<TankEnemyBehaviour>().isDestroyed != true)
+                    {
+                        TankEnemyBehaviour thisTowersTank = transform.parent.GetComponent<TankEnemyBehaviour>();
+                        float destroyTime = thisTowersTank.destroyTime; //+1f = yield wait in destruction function of enemy tank
+                        thisTowersTank.CollisionDetected(this);
+                        Destroy(this.gameObject, destroyTime);
+                    }
+                }
+            }
         }
     }
 }
