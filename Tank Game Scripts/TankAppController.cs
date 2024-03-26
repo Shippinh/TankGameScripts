6d89a573ec3b1c735d5ca2a048ef7c86e396cd1a
Shippinh <logoshit@gmail.com>
2024-03-26 09:39:07 +0200

    Created Singleton class to control Global Audio Mixer

---
+++ Tank Game Scripts/TankAppController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class TankAppController : MonoBehaviour
+{
+    public static TankAppController Instance { get; private set; }
+
+    public AudioMixer masterMixer;
+    public int SoundVolume = 0; //dB
+    public int MusicVolume = 0; //dB
+
+    private void Awake() 
+    { 
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        }
+    }
+
+    public void SetSFXVolume(int value)
+    {
+        masterMixer.SetFloat("SFX", value);
+        SoundVolume = value;
+    }
+
+    public void SetMusicVolume(int value)
+    {
+        masterMixer.SetFloat("Music", value);
+        MusicVolume = value;
+    }
+}
