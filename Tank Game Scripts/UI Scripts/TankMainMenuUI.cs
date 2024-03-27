f47d4b1bfe39ef9b3d625a250da4ba9535ce18ad
Shippinh <logoshit@gmail.com>
2024-03-27 20:32:51 +0200

    Implemented basic shop functionality

--- Tank Game Scripts/UI Scripts/TankMainMenuUI.cs
+++ Tank Game Scripts/UI Scripts/TankMainMenuUI.cs
@@ -17,7 +17,10 @@ public class TankMainMenuUI : MonoBehaviour
 
     void Awake()
     {
-        data = TankDataHandler.LoadAllData(); //doing this in awake cause i have to load values for the shop initially
+        //nevermind, i give up
+        /*data = TankDataHandler.LoadAllData(); //doing this in awake cause i have to load values for the shop initially
+        data.Remove(data.First().Key); //*/
+
         //call app prefs here to load volume data
         GoToMainMenu();
         /*foreach(var element in data)
@@ -28,7 +31,7 @@ public class TankMainMenuUI : MonoBehaviour
 
     void Start()
     {
-        //initializing values for Global Audio Mixer (the one that regulates volumes of Audio Sources (that means i need to include mixer in all audio sources))
+        //initializing values for Global Audio Mixer (the one that regulates volume of Audio Sources (that means i need to include mixer in all audio sources))
         TankAppController.Instance.SetSFXVolume(PlayerPrefs.GetInt("SoundVolume", 0));
         TankAppController.Instance.SetMusicVolume(PlayerPrefs.GetInt("MusicVolume", 0));
     }
@@ -45,19 +48,75 @@ public class TankMainMenuUI : MonoBehaviour
         Button shopButton = mainMenuRoot.Q<Button>("ShopButton");
         Button settingsButton = mainMenuRoot.Q<Button>("SettingsButton");
 
-        playButton.clicked += () => SceneManager.LoadSceneAsync(scenePath + "TankWorld.unity");
+        playButton.clicked += () => SceneManager.LoadSceneAsync(scenePath + "TankWorld.unity");//redo this at some point
         shopButton.clicked += () => GoToShop();
         settingsButton.clicked += () => GoToSettings();
         #endregion
 
         #region Shop Menu
+        Button returnFromShopButton = shopMenuRoot.Q<Button>("BackToMainMenuButton");
+        Label coinsLabel = shopMenuRoot.Q<Label>("CoinsLabel");
+
+        Label ramBonusLabel = shopMenuRoot.Q<Label>("RamBonusLabel");
+        Label thrallBonusLabel = shopMenuRoot.Q<Label>("ThrallBonusLabel");
+        Label armorBonusLabel = shopMenuRoot.Q<Label>("ArmorBonusLabel");
+
+        Label ramCostLabel = shopMenuRoot.Q<Label>("RamCostLabel");
+        Label thrallCostLabel = shopMenuRoot.Q<Label>("ThrallCostLabel");
+        Label armorCostLabel = shopMenuRoot.Q<Label>("ArmorCostLabel");
+
+        Button upgradeRamButton = shopMenuRoot.Q<Button>("UpgradeRamButton");
+        Button upgradeThrallButton = shopMenuRoot.Q<Button>("UpgradeThrallButton");
+        Button upgradeArmorButton = shopMenuRoot.Q<Button>("UpgradeArmorButton");
+        
+        returnFromShopButton.clicked += () => GoToMainMenuAndSave();
+
+        upgradeRamButton.clicked += () => 
+        {
+            if(data["Coins"] >= 20)
+            {
+                data["RamLevel"]++;
+                data["Coins"] -= 20;
+                TankDataHandler.SaveAllData((int)data["Coins"], 0, (int)data["ThrallLevel"], (int)data["RamLevel"]);
+                data = TankDataHandler.LoadAllData(); //super stupid
+                data.Remove(data.First().Key); //i hate this so much
+
+                coinsLabel.text = "Coins: " + data["Coins"];
+
+                ramBonusLabel.text = "Level: " 
+                + data["RamLevel"].ToString() 
+                + "\nCurrent bonuses:\n+" 
+                + data["RamDuration"].ToString() 
+                + " to Duration\n+1 Armor per rammed tank";
+            }
+        };
+
+        upgradeThrallButton.clicked += () => 
+        {
+            if(data["Coins"] >= 20)
+            {
+                data["ThrallLevel"]++;
+                data["Coins"] -= 40;
+                TankDataHandler.SaveAllData((int)data["Coins"], 0, (int)data["ThrallLevel"], (int)data["RamLevel"]);
+                data = TankDataHandler.LoadAllData(); //super stupid
+                data.Remove(data.First().Key); //even dumber
+
+                coinsLabel.text = "Coins: " + data["Coins"];
+
+                thrallBonusLabel.text = "Level: " 
+                + data["ThrallLevel"].ToString() 
+                + "\nCurrent bonuses:\n+" 
+                + data["ThrallDuration"].ToString() + " to Duration";
+            }
+        };
+        
         #endregion
 
         #region Settings Menu
         Button sendToPaymentButton = settingsMenuRoot.Q<Button>("SendToPaymentButton");
-        Button returnButton = settingsMenuRoot.Q<Button>("BackToMainMenuButton");
+        Button returnFromSettingsButton = settingsMenuRoot.Q<Button>("BackToMainMenuButton");
 
-        returnButton.clicked += () => GoToMainMenuAndSave();
+        returnFromSettingsButton.clicked += () => GoToMainMenu();
 
         SliderInt effectsVolumeSliderInt = settingsMenuRoot.Q<SliderInt>("SoundVolume");
         SliderInt musicVolumeSliderInt = settingsMenuRoot.Q<SliderInt>("MusicVolume");
@@ -86,8 +145,30 @@ public class TankMainMenuUI : MonoBehaviour
 
     void GoToShop()
     {
+        data = TankDataHandler.LoadAllData(); //doing this in awake cause i have to load values for the shop initially
+        data.Remove(data.First().Key);
+
+        var shopMenuRoot = shopMenu.rootVisualElement;
         DisableDisplayStyles();
         shopMenu.rootVisualElement.style.display = DisplayStyle.Flex;
+
+        Label coinsLabelInst = shopMenuRoot.Q<Label>("CoinsLabel");
+        coinsLabelInst.text = "Coins: " + data["Coins"].ToString();
+
+        Label ramBonusLabelInst = shopMenuRoot.Q<Label>("RamBonusLabel");
+        Label thrallBonusLabelInst = shopMenuRoot.Q<Label>("ThrallBonusLabel");
+        //Label armorBonusLabelInst = shopMenuRoot.Q<Label>("ArmorBonusLabel");//implement later
+
+        ramBonusLabelInst.text = "Level: " 
+        + data["RamLevel"].ToString() 
+        + "\nCurrent bonuses:\n+" 
+        + data["RamDuration"].ToString() 
+        + " to Duration\n+1 Armor per rammed tank";
+
+        thrallBonusLabelInst.text = "Level: " 
+        + data["ThrallLevel"].ToString() 
+        + "\nCurrent bonuses:\n+" 
+        + data["ThrallDuration"].ToString() + " to Duration";
     }
 
     void GoToSettings()
@@ -107,7 +188,7 @@ public class TankMainMenuUI : MonoBehaviour
         DisableDisplayStyles();
         mainMenu.rootVisualElement.style.display = DisplayStyle.Flex;
         //TankDataHandler.SaveAllData(200, 0, 5, 6);
-        TankDataHandler.SaveAllData(data);
+        TankDataHandler.SaveAllData((int)data["Coins"], 0, (int)data["ThrallLevel"], (int)data["RamLevel"]);
     }
 
     void DisableDisplayStyles()
